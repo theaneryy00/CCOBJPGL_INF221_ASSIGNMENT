dc5829d0dd40757a01892c086e8f4383d05d4eb8
theaneryy00 <111473139+theaneryy00@users.noreply.github.com>
2023-05-16 15:09:12 +0800

    Update Pugzu.java

---
+++ Exercise3/Dog/Pugzu.java
@@ -0,0 +1,11 @@
+class Pugzu extends Pug implements Shitzu {
+
+    String name = "She is my dog Serene";
+    String Serene = "She is a friendly dog";
+
+    public void bark() {
+        System.out.println("My Golden Poodle is barking");
+    }
+
+
+}
