57a25cb3e4fd3f91e4672825a1ea1eb92fb811c8
theaneryy00 <111473139+theaneryy00@users.noreply.github.com>
2023-05-17 20:03:59 +0800

    Update App.java

---
+++ Exercise3/src/Dog/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Create object from pet class
+        Pugzu myPet = new Pugzu();
+
+        System.out.println(myPet.name);
+        System.out.println(myPet.Serene);
+        myPet.bark();
+        System.out.println("She got a " + myPet.hair + myPet.color + "hair and a " myPet.head);
+        System.out.println("She is " + myPet.height2 + " tall.");
+    }
+}
