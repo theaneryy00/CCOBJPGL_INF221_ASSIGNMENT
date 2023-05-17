91945d63bd8b13641f7208c6aeddb1ccdde9d8e9
theaneryy00 <111473139+theaneryy00@users.noreply.github.com>
2023-05-17 20:02:43 +0800

    Update App.java

--- Exercise3/src/Cat/App.java
+++ Exercise3/src/Cat/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Create object from pet class
+        Pet myPet = new Pet();
+
+        System.out.println(myPet.name);
+        System.out.println(myPet.age);
+        myPet.meow();
+        System.out.println("My cat has a " + myPet.eyeshape0 + " eyes and a " + myPet.bodyshape + "chest.");
+        System.out.println("His fur's color is " + mePet.color + "that he got from his parents.");
     }
 }
